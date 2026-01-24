2e6ee652bf87cba1d7318ed15308576a0c6b6b1f
Theo Sementa <theosementa@gmail.com>
2026-01-24 13:13:47 +0100

    feat: add interfaces

---
+++ src/interfaces/IUserRepository.ts
@@ -0,0 +1,9 @@
+import { UserEntity } from "../objects/entities/user.entity";
+
+export interface IUserRepository {
+    findByEmail(email: string): Promise<UserEntity | null>;
+    findById(id: number): Promise<UserEntity | null>;
+    save(user: Partial<UserEntity>): Promise<UserEntity>;
+    updateRefreshToken(id: number, token: string | null): Promise<void>;
+    delete(id: number): Promise<void>;
+}
